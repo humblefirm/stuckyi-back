0b08b5096f42da0dd358715b65bc18e3778c6fe9
humblefirm <38067691+humblefirm@users.noreply.github.com>
2021-12-27 18:12:32 +0900

    init

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+
+var DB = {
+  users: [],
+  goods: [],
+};
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+module.exports = router;
